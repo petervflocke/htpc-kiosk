5e5e045640cc1c70834c0a5d6d5676281878dd2e
petervflocke <23005024+petervflocke@users.noreply.github.com>
2025-08-03 13:21:27 +0200

    chore: remove leftover inline comments

--- dialog-preload.js
+++ dialog-preload.js
@@ -2,5 +2,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('dialogAPI', {
   onDialogOptions: (callback) => ipcRenderer.on('dialog-options', (event, ...args) => callback(...args)),
-  sendResponse: (response) => ipcRenderer.send('dialog-response', response),
+  sendResponse: (id, response) => ipcRenderer.send(`dialog-response-${id}`, response),
 });
